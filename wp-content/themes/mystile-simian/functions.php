088a87977fe5690421b20c4bb9c7bd37943a2aa2
Pablo <pablo@puntomob.com>
2014-07-15 13:10:22 -0500

    [Woo-25]

--- wp-content/themes/mystile-simian/functions.php
+++ wp-content/themes/mystile-simian/functions.php
@@ -1,9 +1,15 @@
 <?php
-function remove_unused_field( $fields){
+function customize_checkout_fields( $fields){
 	unset($fields['billing']['billing_postcode']);
 	unset($fields['billing']['billing_company']);
+	unset($fields['shipping']['shipping_postcode']);
+	unset($fields['shipping']['shipping_company']);
+	$fields['billing']['billing_state']['label'] = __('Department','woocommerce');
+	$fields['billing']['billing_state']['placeholder'] = __('Department','woocommerce');
+	$fields['shipping']['shipping_state']['label'] = __('Department','woocommerce');
+	$fields['shipping']['shipping_state']['placeholder'] = __('Department','woocommerce');
 	return $fields;
 }
-add_filter('woocommerce_checkout_fields','remove_unused_field');
+add_filter('woocommerce_checkout_fields','customize_checkout_fields');
 
 ?>
