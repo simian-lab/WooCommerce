fe09b00124db1510ec792e509921e953717310e8
Pablo <pablo@puntomob.com>
2014-07-22 13:15:28 -0500

    [WOO-27] Added States of Colombia

--- wp-content/themes/mystile-simian/functions.php
+++ wp-content/themes/mystile-simian/functions.php
@@ -12,7 +12,28 @@ function customize_checkout_fields( $fields){
 }
 add_filter('woocommerce_checkout_fields','customize_checkout_fields');
 function enqueue_print_style(){
-	wp_enqueue_style('print', get_stylesheet_directory_uri() . '/css/print.css' );
+	wp_enqueue_style('print', get_stylesheet_directory_uri() . '/css/print.css' );	
 }
-add_action('wp_enqueue_scripts', 'enqueue_print_style');
+add_action('wp_enqueue_scripts', 'enqueue_print_style'); 
+function CO_woocommerce_states( $states ) {
+ 
+  $states['CO'] = array(
+				'CU' => __('Cundinamarca', 'woocommerce') ,
+				'AN' => __('Antioquia', 'woocommerce') ,
+				'ST' => __('Santander', 'woocommerce') ,
+				'VC' => __('Valle Del Cauca', 'woocommerce') ,
+				'AT' => __('Atlántico', 'woocommerce') ,
+				'BO' => __('Bolívar', 'woocommerce') ,
+				'NS' => __('Norte de Santander', 'woocommerce') ,
+				'TO' => __('Tolima', 'woocommerce') ,
+				'MG' => __('Magdalena', 'woocommerce'),
+				'ME' => __('Meta', 'woocommerce'),
+				'RS' => __('Risaralda', 'woocommerce'),
+				'CA' => __('Caldas', 'woocommerce'),
+				'OC' => __('Other Cities', 'woocommerce')
+  );
+ 
+  return $states;
+}
+add_filter( 'woocommerce_states', 'CO_woocommerce_states' );
 ?>
